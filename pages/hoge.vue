91bbd93487592af64811f777adc3a1a8a4963ad3
sakamoto.m <sakamoto.m@water-cell.jp>
2018-07-03 13:01:09 +0900

    最初のリンクを設置

---
+++ pages/hoge.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>page hoge</h1>
+      <nuxt-link to="/" >home</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "hoge",
+  };
+</script>
+
+<style scoped>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -8,15 +8,10 @@
       <h2 class="subtitle">
         Nuxt.js project
       </h2>
+      <h2>page link</h2>
+
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <nuxt-link to="/hoge" class="links">hoge</nuxt-link>
       </div>
     </div>
   </section>
